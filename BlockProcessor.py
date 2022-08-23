7911dad23c792b16ba533ea8b1a19e17a981024c
Akshay Rakheja <a3rakhej@uwaterloo.ca>
2022-08-22 23:52:06 -0400

    feat: initial commit

---
+++ BlockProcessor.py
@@ -0,0 +1,58 @@
+
+
+from curses import has_key
+
+
+# Process Blocks returns accepted Height given startHeight and blocks as a list
+
+#  Q1) Are the blocks that are not accepted, remembered? eg:  block_3 followed by block 1. Should ProcessBlocks return 4 next?
+#  Q2)
+'''
+Assumptions and Thoughts:
+
+1. Blocks are fed in sequence and need to be verified.
+a) They need to be verfied sequentially. This means that cryptographically the has of the current block needs to be connect to the previous block.
+b) The previous block needs to be verified and cannot be the same as the current block.
+
+'''
+
+blocks_counts = {}
+blocks_at_height = []
+current_height = 0
+
+
+def ProcessBlocks(startHeight, blocks):
+    '''
+    For each block in the list, add the block to blocks_at_height if the cryptographic hash is valid (in assumptions).
+    Increment the count for the block
+
+    '''
+    for i, block in enumerate(blocks):
+        if len(blocks_at_height) < startHeight + i:
+            # if verifyHash(block):
+            blocks_at_height.append(block)
+        if block in blocks_counts:
+            blocks_counts[block] += 1
+        else:
+            blocks_counts[block] = 1
+    global current_height
+    total = current_height
+    while total < len(blocks_at_height):
+        if (blocks_counts[blocks_at_height[total]] < 3):
+            break
+        total += 1
+    current_height = total
+    return current_height
+
+
+# Verify whether block is one character greater than the last item at blocks_at_height
+
+
+def verifyHash(block):
+    if len(blocks_at_height) > 0:
+        if block == blocks_at_height[-1] + chr(1):
+            return True
+        else:
+            return False
+    else:
+        return True
---
+++ test.py
@@ -0,0 +1,25 @@
+from BlockProcessor import ProcessBlocks
+
+
+block_1 = ['A', 'B', 'C', 'D']
+block_2 = ['A', 'B', 'C', 'D']
+block_3 = ['A', 'B', 'C']
+block_4 = ['A', 'B', 'C', 'D']
+block_5 = ['E', 'F', 'G']
+block_6 = ['E', 'F', 'G']
+block_7 = ['E', 'F', 'G']
+block_8 = ['E', 'F', 'G']
+block_9 = ['E', 'F', 'G']
+
+
+res_1 = ProcessBlocks(1, block_1)
+res_2 = ProcessBlocks(1, block_2)
+res_3 = ProcessBlocks(1, block_3)
+res_4 = ProcessBlocks(1, block_4)
+res_5 = ProcessBlocks(1, block_5)
+res_6 = ProcessBlocks(1, block_6)
+res_7 = ProcessBlocks(1, block_7)
+res_8 = ProcessBlocks(1, block_8)
+res_9 = ProcessBlocks(1, block_9)
+
+print(res_1, res_2, res_3, res_4, res_5, res_6, res_7, res_8, res_9)
